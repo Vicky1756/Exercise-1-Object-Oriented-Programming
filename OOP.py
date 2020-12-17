9a61b3cff82fe148ee733b5d5aed68ded5d3ecd3
Vicky1756 <auraiwan1756@gmail.com>
2020-12-17 22:15:30 +0700

    first commit

---
+++ OOP.py
@@ -0,0 +1,19 @@
+# Write a Python class named Circle constructed by a radius and two methods which will compute the area and the perimeter of a circle.
+import math
+
+
+class Circle():
+
+    def __init__(self, r):
+        self.radius = r
+
+    def area(self):
+        return (math.pi)*self.radius**2
+
+    def perimeter(self):
+        return 2*(math.pi)*self.radius
+
+
+TestCircle = Circle(20)
+print(TestCircle.area())
+print(TestCircle.perimeter())
